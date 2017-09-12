2f6da1a5f41f976b74b62c78a8126de92c101759
runninghack <zl.ramiel@gmail.com>
2017-09-11 21:38:13 -0400

    correct TR40

--- keyboard/TR40/keymap_default.c
+++ keyboard/TR40/keymap_default.c
@@ -1,21 +1,18 @@
 #include "keymap_common.h"
 
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
-
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     /* 0: Default Layer */
     KEYMAP(
-    		  HPR_TAB, Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC, \
-    		  CTL_ESC, A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT, \
-    		  LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH, SFT_ENT, \
-    		  MEH_GRV, LCTL, LALT, LGUI, LOWER,   SPC,  SPC,  RAISE,   LEFT, DOWN, UP,   RGHT),
+    		  TAB, Q,    W,    E,    R,    T,    Y,    U,    I,    O,    P,    BSPC, \
+    		  ESC, A,    S,    D,    F,    G,    H,    J,    K,    L,    SCLN, QUOT, \
+    		  LSFT, Z,    X,    C,    V,    B,    N,    M,    COMM, DOT,  SLSH, ENT, \
+    		  GRV, LCTL, LALT, LGUI, FN0,   SPC,  SPC,  FN1,   LEFT, DOWN, UP,   RGHT),
     /* 1: Lower Layer */
     KEYMAP(
-    		0,          1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    DEL, \
-			_______,    F1,   F2,   F3,   F4,   F5,   F6,   4,    5,    6,    _______, _______, \
-    		_______,    F7,   F8,   F9,   F10,  F11,  F12,  1,    2,    3,    _______, _______, \
-			_______,    _______, _______, _______, _______, _______, _______, _______, MNXT, VOLD, VOLU, MPLY),
+    		0,       1,    2,    3,    4,    5,    6,    7,    8,    9,    0,    DEL, \
+		TRNS,    F1,   F2,   F3,   F4,   F5,   F6,   4,    5,    6,    TRNS, TRNS, \
+    		TRNS,    F7,   F8,   F9,   F10,  F11,  F12,  1,    2,    3,    TRNS, TRNS, \
+		TRNS,    TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, MNXT, VOLD, VOLU, MPLY),
 };
 
 
