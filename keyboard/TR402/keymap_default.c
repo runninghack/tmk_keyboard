1f4b730ebf234eee38440f05288a86a9c9a9546c
runninghack <zl.ramiel@gmail.com>
2017-09-13 22:07:44 -0400

    update tr402

--- keyboard/TR402/keymap_default.c
+++ keyboard/TR402/keymap_default.c
@@ -1,4 +1,9 @@
+#include <avr/pgmspace.h>
+#include "keycode.h"
+#include "action.h"
+#include "action_util.h"
 #include "keymap_common.h"
+
 
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
     /* 0: Default Layer */
@@ -16,23 +21,9 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 };
 
 
-#ifdef KEYMAP_SECTION_ENABLE
-const uint16_t fn_actions[FN_ACTIONS_COUNT] __attribute__ ((section (".keymap.fn_actions"))) = {
-#else
 const uint16_t fn_actions[] PROGMEM = {
-#endif
     [0] = ACTION_LAYER_MOMENTARY(1),                // to Fn overlay
     [1] = ACTION_BACKLIGHT_TOGGLE(),                // BackLight TOGGLE 
     [2] = ACTION_BACKLIGHT_DECREASE(),              // BackLight -      
     [3] = ACTION_BACKLIGHT_INCREASE(),              // BackLight +      
 };
-
-#ifdef KEYMAP_IN_EEPROM_ENABLE
-uint16_t keys_count(void) {
-    return sizeof(keymaps) / sizeof(keymaps[0]) * MATRIX_ROWS * MATRIX_COLS;
-}
-
-uint16_t fn_actions_count(void) {
-    return sizeof(fn_actions) / sizeof(fn_actions[0]);
-}
-#endif
